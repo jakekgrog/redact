1956dc0254ad71401c7985382c7820586c7d913f
r-dog <Jake.kgrogan@gmail.com>
2018-04-09 00:42:19 +0100

    added upload image

---
+++ main.js
@@ -0,0 +1,50 @@
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+const shell = require('electron').shell;
+
+const {app, BrowserWindow, ipcMain, Menu, Tray, webContents} = electron;
+
+let mainWindow;
+
+// Listen for app to be ready
+app.on('ready', function(){
+    //Create new window
+    mainWindow = new BrowserWindow({});
+    // Load html into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, "src/mainWindow.html"),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    mainWindow.webContents.toggleDevTools();
+
+    var menu = Menu.buildFromTemplate([
+        {
+            label: 'Menu',
+            submenu: [
+                {
+                    label: 'Exit',
+                    click() {
+                        app.quit();
+                    }
+                },
+            ]
+        },
+        {
+            label: 'Info',
+            submenu: [
+                {
+                    label: 'View Source',
+                    click() {
+                        shell.openExternal("https://www.github.com/r-dog/redact");
+                    }
+                }
+            ]
+        }
+    ]);
+
+    Menu.setApplicationMenu(menu);
+
+});
